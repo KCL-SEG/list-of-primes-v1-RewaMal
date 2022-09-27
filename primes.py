d27a37addcec858920bb0fff5c11798caab93a59
rewamal <rewa.mal@kcl.ac.uk>
2022-09-27 19:22:34 +0100

    edit solution for primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,11 @@
 
 def primes(number_of_primes):
     list = []
+    prime_num =[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+    for x in range(0,number_of_primes):
+        list.append(prime_num[x])
+
     return list
+    
+print(primes(20))
